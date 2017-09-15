60c2e95e0c17e717749925d26820e5a45498c8e3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-09-16 00:05:49 +0900

    Commit React Components for page

---
+++ src/components/Counter/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Actions from '../../actions/counter';
+import * as Stores from '../../stores/counter';
+
+export default class Counter extends React.Component {
+    static CLASS_NAME = 'Counter';
+
+    static propTypes = {
+        match : PropTypes.object
+    };
+
+    constructor(props) {
+        super(props);
+
+        const count = (props.match && props.match.params) ? parseInt(props.match.params.count) : 0;
+
+        this.state = {
+            count : isNaN(count) ? 0 : count
+        };
+    }
+
+    onClickUpButton() {
+        // TODO
+    }
+
+    onClickDownButton() {
+        // TODO
+    }
+
+    render() {
+        const { count } = this.state;
+
+        return (
+          <div className={Counter.CLASS_NAME}>
+              <p>
+                  <button type="button" className={`${Counter.CLASS_NAME}__up Button`} onClick={this.onClickUpButton.bind(this)}>+</button>
+                  <button type="button" className={`${Counter.CLASS_NAME}__down Button`} onClick={this.onClickDownButton.bind(this)}>-</button>
+              </p>
+              <p>
+                  <span className={`${Counter.CLASS_NAME}__count`}>{count}</span>
+              </p>
+          </div>
+        );
+    }
+}
