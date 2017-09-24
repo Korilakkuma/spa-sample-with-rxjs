182ba72edefc4f7290e678d95aad76711c389279
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-09-24 18:00:00 +0900

    Update Store

--- src/stores/index.js
+++ src/stores/index.js
@@ -7,10 +7,16 @@ export default class Store extends Dispatcher {
         super();
         this.count = 0;
 
-        dispatcher.on('COUNT_UP', this.onCountUp.bind(this));
+        dispatcher.on('COUNT_UP',   this.onCountUp.bind(this));
+        dispatcher.on('COUNT_DOWN', this.onCountDown.bind(this));
     }
 
     onCountUp(count) {
+        this.count = count;
+        this.emit('CHANGE');
+    }
+
+    onCountDown(count) {
         this.count = count;
         this.emit('CHANGE');
     }
