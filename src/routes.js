857474ba93c43d544c0c1a2f065065660b77a7c8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-09-29 10:46:31 +0900

    Update Routing

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,14 @@
 'use strict';
 
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import Nav from './components/Nav';
 import Counter from './components/Counter';
 
 export default (
     <nav>
-        <ul>
-            <li><Link to="/">HOME</Link></li>
-            <li><Link to="/counter">COUNTER from 0</Link></li>
-            <li><Link to="/counter/100">COUNTER from 100</Link></li>
-        </ul>
-
+        <Nav />
         <Route exact path="/" component={Home} />
         <Route exact path="/counter" component={Counter} />
         <Route path="/counter/:count" component={Counter} />
